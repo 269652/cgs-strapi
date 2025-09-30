2779f6cb250248718b3411428dba5aa663ed37ba
Moritz Roessler <moritz.roessler@digitas.com>
2025-10-01 00:51:41 +0200

    fix: try non secure session for railway

--- config/server.ts
+++ config/server.ts
@@ -1,8 +1,9 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://your-app.up.railway.app'),
+  proxy: true, // trust x-forwarded-* headers
   app: {
     keys: env.array('APP_KEYS'),
   },
-  proxy: true
 });
